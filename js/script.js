c068481647c06797e06ea71fc20fd252710647d7
celestine <lestine603@gmail.com>
2023-08-23 17:16:50 +0100

    Adding some functionalities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Hamburger menu for mobile
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
@@ -9,11 +10,77 @@ function navToggle() {
     menu.classList.toggle("hidden")
 }
 
-// const navElements = document.querySelector('.hi');
 
-// console.log(navElements.children[1])
+// active states for nav menu (Desktop view)
+function setUpEvents() {
+    const desktop = document.querySelector(".desktop");
+    console.log(desktop);
+
+    const newDiv2 = document.createElement("div");
+    newDiv2.setAttribute("class", "line2");
+    newDiv2.innerHTML = `
+        <div class="linear2"></div>
+    `
+
+    desktop.addEventListener("click", e => {
+        e.preventDefault();
+
+        if (e.target.classList.contains("navigation")) {
+            e.target.parentElement.append(newDiv2);
+        }
+    })
+    console.log("Yea, I did it!")
+}
+
+// setUpEvents()
+
+// window.onload = function() {
+//     setUpEvents();
+// }
+
+// window.onload = setUpEvents;
+
+// window.onload = function() {
+//     var reloading = sessionStorage.getItem("reloading");
+//     if (reloading) {
+//         sessionStorage.removeItem("reloading");
+//         setUpEvents();
+//     }
+// }
+
+// function reloadP() {
+//     sessionStorage.setItem("reloading", "true");
+//     document.location.reload();
+// }
 
 
-// const anch = document.querySelector('.uni');
 
-// console.log(anch.firstElementChild.baseURI);
+// consuming local json data
+
+// automatic  slide for the crew members
+
+// active states for choosing of planets on destination page
+const galaxies = document.querySelector(".galaxy");
+
+const newDiv = document.createElement("div");
+newDiv.setAttribute("class", "line");
+
+newDiv.innerHTML = `
+<div class="linear"></div>
+`
+
+galaxies.addEventListener("click", e => {
+    
+    if (e.target.classList.contains("planet")) {
+        e.target.parentElement.append(newDiv); 
+    };
+})
+
+// const desktop = document.querySelector(".desktop");
+
+// const newDiv2 = document.createElement("div");
+// newDiv.setAttribute("class", "line2");
+// newDiv2.innerHTML = `
+// <div class="linear2"></div>
+// `
+
